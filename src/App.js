33a4db7b2ced08ecfdf1e498cb3de0dd9cb7c352
JagadeeswariPeddi <chinnipeddi3126@gmail.com>
2022-11-30 12:02:55 +0530

    component

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/*import ClassComponent from "./ClassComponent"
+import ClassComponent1 from "./ClassComponent1"
+function Demo(){
+  return(
+   <section>
+   <ClassComponent1/>
+   <img src="https://images7.alphacoders.com/411/411820.jpg" alt="nature"  width="40%"/>
+   <p>Hello fellows</p>
+   <h1>This is h1 tag</h1>
+   <ClassComponent/>
+   </section>
+   )
+}
+export default Demo*/
+import PropsExample from "./PropsExample"
+function Demo(){
+  return(
+    <div>
+    <h1>This is about props example</h1>
+    <PropsExample name="chinni" age="20"/>
+    </div>
+    )
+}
+export default Demo
